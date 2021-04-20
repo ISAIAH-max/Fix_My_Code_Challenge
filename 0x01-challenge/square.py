ea2de48f251eb091fd4ab0031d293fbed8f8b864
ISAIAH-max <mlewaisaiah@gmail.com>
2021-04-20 03:55:41 +0300

    1. My square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -22,7 +22,7 @@ class square():
 
 if __name__ == "__main__":
 
-    s = square(width=16, height=9)
+    s = square(width=81, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
