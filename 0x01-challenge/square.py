53faffa9124e20b39575d3b10d35e466ccceb84d
ISAIAH-max <mlewaisaiah@gmail.com>
2021-04-20 03:51:00 +0300

    1. My square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -12,19 +12,17 @@ class square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        if (self.width == self.height):
-            return self.width * self.width
+        return self.width * self.width
 
     def PermiterOfMySquare(self):
-        if (self.width == self.height):
-            return (self.width * 2) + (self.height * 2)
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = square(width=16, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
